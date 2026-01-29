40f939127fee999a03ec5a07dd6303c79671c027
Md Badrul Alam <developer.badrul@gmail.com>
2026-01-29 16:52:00 +0600

    setup Axios private hook

---
+++ src/hooks/usePrivateAxios.js
@@ -0,0 +1,51 @@
+import { useEffect } from "react";
+import useAuth from "./useAuth";
+import { privateApi, publicApi } from "../apis/axios";
+
+const usePrivateAxios = () => {
+    const { auth, setAuth } = useAuth();
+
+    useEffect(() => {
+        // Add a request interceptor for private apis
+        const requestIntercept = privateApi.interceptors.request.use(
+            (config) => {
+                const authToken = auth?.authToken;
+                if (authToken) {
+                    config.headers.Authorization = `Bearer ${authToken}`;
+                }
+                return config;
+            },
+
+            (error) => Promise.reject(error)
+        )
+
+        // Add a response interceptor for private apis
+        const responseIntercept = privateApi.interceptors.response.use(
+            (response) => response,
+            async (error) => {
+                const originalRequest = error.config;
+
+                if (error.response.status === 401 && !originalRequest._retry) {
+                    originalRequest._retry = true;
+
+                    try {
+                        const refreshToken = auth?.refreshToken;
+
+                        const response = await publicApi.post(`/auth/refresh-token`, { refreshToken });
+                        const { token } = response.data;
+                        console.log(`New Token: ${token}`);
+
+                        setAuth({ ...auth, authToken: token });
+                    } catch (error) {
+
+                    }
+                }
+            }
+
+        )
+
+    }, [])
+
+};
+
+export default usePrivateAxios;
