d7f43e5323944f1b9c53510b25047de6094744e6
Md Badrul Alam <developer.badrul@gmail.com>
2026-02-09 13:37:42 +0600

    add hook for avater

--- src/utils/index.js
+++ src/utils/index.js
@@ -8,3 +8,39 @@ export const getChildId = (children) => {
         return child.props.id
     }
 }
+
+
+
+export const getDateDifferenceFromNow = (fromDate) => {
+    const now = Date.now();
+    const target = new Date(fromDate).getTime();
+
+    if (isNaN(target)) return "Invalid Date";
+
+    let diff = Math.floor((now - target) / 1000); // second
+    const isPast = diff < 0;
+    diff = Math.abs(diff);
+
+    const units = [
+        { label: 'day', value: 86400 },
+        { label: 'hour', value: 3600 },
+        { label: 'minute', value: 60 },
+        { label: 'second', value: 1 },
+    ];
+
+    const parts = [];
+
+    for (const unit of units) {
+        const amount = Math.floor(diff / unit.value);
+        if (amount > 0) {
+            parts.push(`${amount} ${unit.label}${amount > 1 ? 's' : ''}`)
+            diff %= unit.value
+        }
+
+        if (parts.length === 2) break;
+    }
+
+    if (parts.length === 0) return 'just now';
+
+    return isPast ? `${parts.join(" ")} ago` : `in ${parts.join(" ")}`;
+};
