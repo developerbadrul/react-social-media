1688478af320568cb76c6827866fd23328123d1a
Md Badrul Alam <developer.badrul@gmail.com>
2026-02-14 12:16:26 +0600

    posts sorted

--- src/components/Posts/PostList.jsx
+++ src/components/Posts/PostList.jsx
@@ -1,8 +1,13 @@
 import PostCard from "./PostCard";
 
 const PostList = ({ posts }) => {
+    if (!posts || posts.length === 0) return;
+
+    const sortedPosts = Array.from(posts).sort((a, b) => {
+        return new Date(b.createAt) - new Date(a.createAt)
+    })
     return (
-        !!posts && posts.map((post) => <PostCard key={post.id} post={post} />)
+        sortedPosts.map((post) => <PostCard key={post.id} post={post} />)
     );
 };
 
